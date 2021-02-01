709dee269e3877c53211df3f637cf0436a0a066a
Veer <40777269+V3RS@users.noreply.github.com>
2021-02-01 16:29:06 -0500

    started creating users.js and created user model

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,11 @@
 var express = require('express');
+const bcrypt = require('brcyptjs');
 var router = express.Router();
+const { check } = require('express-validator');
+const { asyncHandler, handleValidationErrors } = require("../utils");
+const db = require("../db/models");
+
+const { User } = db;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
