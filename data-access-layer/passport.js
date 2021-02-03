0ef59ec46eda2d473f10fcc64d6b820bc026a446
raymondmay95 <raymond.may.95@outlook.com>
2021-02-03 10:43:24 -1000

    finished getUserPassports

--- data-access-layer/passport.js
+++ data-access-layer/passport.js
@@ -1,1 +1,14 @@
 const { User, Passport } = require("../db/models");
+
+const getUserPassports = async (id) => {
+   const passports = await findAll({
+      where: {
+         user_id = id
+      }
+   })
+   return passports
+}
+
+module.exports = {
+   getUserPassports,
+}
