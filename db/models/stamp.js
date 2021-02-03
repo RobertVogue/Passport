d72f7cb3f1c6247562a85aa4a8059fe277c442d2
raymondmay95 <raymond.may.95@outlook.com>
2021-02-03 01:37:45 -1000

    seeders are done we just have a fk conflict on passports. please review associations

--- db/models/stamp.js
+++ db/models/stamp.js
@@ -1,12 +1,43 @@
 "use strict";
+
+const { Sequelize } = require("sequelize/types");
+
 module.exports = (sequelize, DataTypes) => {
-  const Stamp = sequelize.define(
-    "Stamp",
-    {
-      name: DataTypes.STRING,
+  const Stamp = sequelize.define("Stamp", {
+    name: {
+      allowNull: false,
+      type: Sequelize.STRING(20),
     },
-    {}
-  );
+    detailed_location: {
+      allowNull: false,
+      type: Sequelize.STRING(50),
+    },
+    passport_id: {
+      type: Sequelize.INTEGER,
+    },
+    countries_id: {
+      type: Sequelize.INTEGER,
+    },
+    tags_id: {
+      type: DataTypes.ARRAY(sequelize.INTEGER),
+    },
+    dates: {
+      type: Sequelize.STRING,
+    },
+    price: {
+      type: Sequelize.STRING,
+    },
+    review: {
+      type: Sequelize.STRING,
+    },
+    rating: {
+      type: Sequelize.INTEGER,
+    },
+    imgURL: {
+      type: Sequelize.STRING,
+    },
+  });
+
   Stamp.associate = function (models) {
     Stamp.belongsTo(models.Passport, { foreignKey: "passport_id" });
     Stamp.belongsTo(models.Stamp_Tag, { foreignKey: "tags_id" });
