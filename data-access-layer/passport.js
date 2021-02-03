b9e6af3308d3e4c52ead314b87bf18483808fa89
raymondmay95 <raymond.may.95@outlook.com>
2021-02-03 10:54:19 -1000

    finished getting user passport by status functions

--- data-access-layer/passport.js
+++ data-access-layer/passport.js
@@ -1,7 +1,7 @@
 const { User, Passport } = require("../db/models");
 
 const getUserPassports = async (id) => {
-   const passports = await findAll({
+   const passports = await Passport.findAll({
       where: {
          user_id = id
       }
@@ -9,6 +9,38 @@ const getUserPassports = async (id) => {
    return passports
 }
 
+const getUserPassportSV = async (id) => {
+      const passport = await Passport.findOne({
+      where: {
+         user_id = id,
+         passport_status = "Visited"
+      }
+   })
+return passport
+}
+const getUserPassportSWT = async (id) => {
+      const passport = await Passport.findOne({
+      where: {
+         user_id = id,
+         passport_status = "Want to visit"
+      }
+   })
+return passport
+}
+const getUserPassportSNB = async (id) => {
+      const passport = await Passport.findOne({
+      where: {
+         user_id = id,
+         passport_status = "Near By"
+      }
+   })
+return passport
+}
+
+
 module.exports = {
    getUserPassports,
+   getUserPassportSV,
+   getUserPassportSWT,
+   getUserPassportSNB
 }
