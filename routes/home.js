d176f834387fb91324cb64ff6d65ef1c88be47d9
Peter Anderson <peterburtonanderson@gmail.com>
2021-02-03 00:03:35 -0700

    navbar structure complete

--- routes/home.js
+++ routes/home.js
@@ -7,4 +7,24 @@ router.get('/', (req, res) => {
     res.render('home', {})
 })
 
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    //Toggle Nav
+    burger.addEventListener('click', e => {
+        nav.classList.toggle('nav-active');
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`
+            }
+        })
+    });
+}
+
+navSlide();
+
 module.exports = router;
