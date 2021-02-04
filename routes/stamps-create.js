584e96482a7dd43405b99f0e51eea5e81d99463c
Peter Anderson <peterburtonanderson@gmail.com>
2021-02-04 10:00:35 -0700

    work on grid layout

--- routes/stamps-create.js
+++ routes/stamps-create.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { asyncHandler, csrfProtection } = require("./utils");
-const { Country } = require('../db/models/country');
+// const { Country } = require('../db/models/country');
 const db = require("../db/models");
 
 router.get("/", asyncHandler(async (req, res) => {
-  // const countries = await Country.findAll();
+  const countries = await db.Country.findAll();
   // console.log("countries-------->>", countries)
-  res.render("stamps-create", {  });
+  res.render("stamps-create", { countries });
 }));
 
 // router.post("/", csrfProtection, asyncHandler (async (req, res) => {
