6a55cc1619d3dfb29cde5dec060adc5bf2984bcd
Peter Anderson <peterburtonanderson@gmail.com>
2021-02-03 23:05:34 -0700

    add route for countries dropdown

--- routes/stamps-create.js
+++ routes/stamps-create.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { asyncHandler, csrfProtection } = require("./utils");
+const { Country } = require('../db/models/country');
 
 router.get("/", (req, res) => {
-  res.render("stamps-create", {});
+  const countries = await Country.findAll()
+  res.render("stamps-create", { countries });
 });
+
+// router.post("/", csrfProtection, asyncHandler (async (req, res) => {
+// }));
 
 module.exports = router;
