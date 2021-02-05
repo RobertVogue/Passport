505cde7cc3fc036356013b01a83a4d4dfca3c505
Peter Anderson <peterburtonanderson@gmail.com>
2021-02-04 21:43:58 -0700

    test background image

--- routes/stamps-create.js
+++ routes/stamps-create.js
@@ -10,7 +10,15 @@ router.get("/create", csrfProtection, asyncHandler(async (req, res) => {
 }));
 
 router.post("/create", csrfProtection, asyncHandler (async (req, res) => {
-  // const {  }
+  console.log("req.body------>>", req.body)
+  //combine date
+  //query for country --> name
+  //query for tag --> name
+  //query for status --> passport_status
+  // const { status, countries, detailed_location, start, end, detailed_description, tag } = req.body;
+  // await db.Stamp.create({ status, countries, detailed_location, start, end, detailed_description, tag });
+
+  res.redirect('/stamps/create');
 }));
 
 router.post("/edit")
