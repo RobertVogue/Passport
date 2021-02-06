f8d3e4eba102db8264affb16e5486ec2dd868833
Peter Anderson <peterburtonanderson@gmail.com>
2021-02-06 13:37:41 -0700

    figured out how to get the country names to display.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var router = express.Router();
-const { csrfProtection } = require("./utils");
+const { csrfProtection, asyncHandler } = require("./utils");
+const db = require('../db/models');
+const { Passport, Country } = require('../db/models')
 
 /* GET home page. */
-router.get('/', csrfProtection, function(req, res, next) {
-  res.render('index', { csrfToken: req.csrfToken() });
-});
+// router.get('/', csrfProtection, function(req, res, next) {
+//   res.render('index', { csrfToken: req.csrfToken() });
+// });
 
+// Query all Countries and count ratings. Put Country as key and count as value.
+// Find Top 3 countries who have the highest counts
 
+router.get('/', csrfProtection, asyncHandler(async (req, res) => {
+  const topTags = await db.Stamp.findAll({
+    where: { rating: 5 },
+    include: [{
+      model: Passport,
+      where: { passport_status: "Want to visit" },
+    },
+    {
+      model: Country
+    }
+  ],
+    limit: 10
+  });
+  console.log("topTags------>", topTags[0])
+  res.render('index', { csrfToken: req.csrfToken(), topTags })
+}))
 
 module.exports = router;
