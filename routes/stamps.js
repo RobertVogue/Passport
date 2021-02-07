d3a5bd7de87307806e2e2460a142e60c5f428b14
Veer <40777269+V3RS@users.noreply.github.com>
2021-02-07 00:19:44 -0500

    layout for view stamp page

--- routes/stamps.js
+++ routes/stamps.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const { asyncHandler, csrfProtection } = require("./utils");
 const db = require("../db/models")
 
-router.get( ":id(\\d+)", asyncHandler(async (req, res) => {
+router.get( "/:id(\\d+)", asyncHandler(async (req, res) => {
     const stampId = req.params.id;
 
     const stamp = await db.Stamp.findByPk(stampId);
 
-    res.render("view-stamp", { stamp });
+    const passport = await db.Passport.findByPk(stamp.passport_id);
+
+    res.render("view-stamp", { stamp, passport });
   })
 );
 
