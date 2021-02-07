717869fa6db683fff431c55dc228f3a76bd1c414
Veer <40777269+V3RS@users.noreply.github.com>
2021-02-07 18:56:13 -0500

    test for indiv country page

--- routes/all-countries.js
+++ routes/all-countries.js
@@ -4,7 +4,23 @@ const { asyncHandler, csrfProtection } = require("./utils");
 const db = require("../db/models")
 
 router.get("/", asyncHandler(async (req, res) => {
-    res.render('all-countries', {} );
+    const countries = await db.Country.findAll();
+    res.render('all-countries', { countries } );
 }))
+
+router.get( "/:id(\\d+)", asyncHandler(async (req, res) => {
+    const countryId = req.params.id;
+    const country = await db.Country.findByPk(countryId);
+
+    const stamps = await db.Stamp.findAll({
+        where: {
+            countries_id: countryId
+        }
+    });
+
+    console.log(stamps);
+
+    res.render('country', { country, stamps })
+}));
 
 module.exports = router;
