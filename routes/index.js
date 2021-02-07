31e508b731f087c04a91a60f61059f085f670c3a
Robert Vogtritter <RCVogtritter@gmail.com>
2021-02-06 18:58:01 -0700

    so much

--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,18 @@ const topRatedCountries = async () => {
 topRatedCountries();
 
 router.get('/', csrfProtection, asyncHandler(async (req, res) => {
-  const topCountries = await db.Stamp.findAll({
+  const topCountriesV = await db.Stamp.findAll({
+    where: { rating: 5 },
+    include: [{
+      model: Passport,
+      where: { passport_status: "Visited" },
+    },
+    {
+      model: Country
+    }
+    ],
+  });
+  const topCountriesW = await db.Stamp.findAll({
     where: { rating: 5 },
     include: [{
       model: Passport,
@@ -46,27 +57,75 @@ router.get('/', csrfProtection, asyncHandler(async (req, res) => {
     }
     ],
   });
-  const countryObj = {};
+  const topCountriesN = await db.Stamp.findAll({
+    where: { rating: 5 },
+    include: [{
+      model: Passport,
+      where: { passport_status: "Near By" },
+    },
+    {
+      model: Country
+    }
+    ],
+  });
+  const countryObjV = {};
+  const countryObjW = {};
+  const countryObjN = {};
 
-  const countryNames = topCountries.map(data => {
+  const countryNamesV = topCountriesV.map(data => {
     return data.Country.dataValues.name
   });
-  console.log(countryNames);
+  console.log(countryNamesV);
 
-  countryNames.forEach(country => {
-    if (countryObj[country]) {
-      countryObj[country]++;
+  countryNamesV.forEach(country => {
+    if (countryObjV[country]) {
+      countryObjV[country]++;
     } else {
-      countryObj[country] = 1;
+      countryObjV[country] = 1;
+    };
+  });
+  
+  const countryNamesW = topCountriesW.map(data => {
+    return data.Country.dataValues.name
+  });
+  console.log(countryNamesW);
+
+  countryNamesW.forEach(country => {
+    if (countryObjW[country]) {
+      countryObjW[country]++;
+    } else {
+      countryObjW[country] = 1;
     };
   });
 
-  let countriesArr = Object.entries(countryObj).sort((a,b)=> b[1] - a[1]).map(el => el[0])
-  countriesArr = countriesArr.splice(0, 10)
-  console.log("countriesArr------------->>>>>>", countriesArr)
+  const countryNamesN = topCountriesN.map(data => {
+    return data.Country.dataValues.name
+  });
+  console.log(countryNamesN);
+
+  countryNamesN.forEach(country => {
+    if (countryObjN[country]) {
+      countryObjN[country]++;
+    } else {
+      countryObjN[country] = 1;
+    };
+  });
+
+  let countriesArrW = Object.entries(countryObjW).sort((a,b)=> b[1] - a[1]).map(el => el[0])
+  countriesArrW = countriesArrW.splice(0, 10)
+  console.log("countriesArrW------------->>>>>>", countriesArrW)
+
+  let countriesArrV = Object.entries(countryObjV).sort((a,b)=> b[1] - a[1]).map(el => el[0])
+  countriesArrV = countriesArrV.splice(0, 10)
+  console.log("countriesArrV------------->>>>>>", countriesArrV)
+
+  let countriesArrN = Object.entries(countryObjN).sort((a,b)=> b[1] - a[1]).map(el => el[0])
+  countriesArrN = countriesArrN.splice(0, 10)
+  console.log("countriesArrN------------->>>>>>", countriesArrN)
+
   // Iterate through object
   // console.log(countryArr);
-  res.render('index', { csrfToken: req.csrfToken(), countriesArr });
+  res.render('index', { csrfToken: req.csrfToken(), countriesArrW, countriesArrV, countriesArrN });
 }));
 
 module.exports = router;
